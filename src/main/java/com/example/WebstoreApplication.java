e4c60ba01ea289f7c25c77e9248034fd9c8aee23
adam <abugajski>
2017-03-07 18:05:57 +0100

    domain from old project

--- src/main/java/com/example/WebstoreApplication.java
+++ src/main/java/com/example/WebstoreApplication.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import domain.Address;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses=Address.class)
 public class WebstoreApplication {
 
 	public static void main(String[] args) {
