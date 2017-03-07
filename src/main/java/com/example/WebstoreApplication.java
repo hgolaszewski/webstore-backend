ebe8c3ba0c92cd505907ae09e89b2f347d7d66d5
Hubert Gołaszewski (mint) <h.golaszewski@gmail.com>
2017-03-07 18:07:50 +0100

    hibernate conf

--- src/main/java/com/example/WebstoreApplication.java
+++ src/main/java/com/example/WebstoreApplication.java
@@ -1,9 +1,14 @@
 package com.example;
 
+import domain.Address;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = Address.class)
 public class WebstoreApplication {
 
 	public static void main(String[] args) {
