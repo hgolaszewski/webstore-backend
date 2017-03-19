ccc64cdba31a36df861b4bebf7c2cac47391e628
Hubert Gołaszewski (mint) <h.golaszewski@gmail.com>
2017-03-19 23:53:42 +0100

    Add password hashing for customer registration

--- src/main/java/com/example/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/service/impl/CustomerServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by hubertus on 3/16/17.
@@ -20,6 +22,17 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Customer addCustomer(Customer customer) {
+        try {
+            MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
+            byte[] result = mDigest.digest(customer.getPassword().getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < result.length; i++) {
+                sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            customer.setPassword(sb.toString());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
         return customerRepository.save(customer);
     }
 
